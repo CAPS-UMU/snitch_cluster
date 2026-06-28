783beb94e4c6c50aa2db5a9ef801a8cff8bf79a8
EmilySillars <emsillars@gmail.com>
2026-06-28 22:38:57 +0200

    actually remove false positives

--- myrtle-experiments/check.py
+++ myrtle-experiments/check.py
@@ -24,7 +24,11 @@ def main():
           subprocess.call(['tail', f"{txtFileDir}both.txt", "-c",f'{len(file_content)//2}' ],stdout=fr)
           fr.close()
          # subprocess.call(['cat', f"{txtFileDir}right.txt" ])
-          return subprocess.call(['diff',  f"{txtFileDir}left.txt",f"{txtFileDir}right.txt" ])
+          result =  subprocess.call(['diff',  f"{txtFileDir}left.txt",f"{txtFileDir}right.txt" ])
+          if result == 0:
+               sys.exit(1)
+          else:
+               sys.exit(0)
      except ValueError:
           print("Possible Error Detected! Re-run this verification script.")
           raise Exception("Possible Error Detected! Re-run this verification script.")
